aeed324d61a88027dabb194eb8d34226815f39a6
mai212228 <mai212228@gmail.com>
2023-11-08 19:05:38 -0500

    Fixed minimax algorithm and optimized board class

--- main.py
+++ main.py
@@ -1,166 +1,148 @@
+import numpy as np
 import random
-import math 
-import copy
+import sys
+import math
 
 class ConnectFour:
-    def __init__(self, rows=6, cols=7):
+    ## SET BOARD PARAMETERS HERE, CAN PLAY AROUND WITH THEM IN DEEP NEURAL NET POSSIBLY
+    def __init__(self, rows=6, columns=7):
         self.rows = rows
-        self.cols = cols
-        self.board = [['-' for _ in range(cols)] for _ in range(rows)]
-        self.turn = 'X'  # Player X goes first
+        self.columns = columns
+        self.USER = 0
+        self.AI = 1
+        self.EMPTY = 0
+        self.USER_PIECE = 1
+        self.AI_PIECE = 2
+        self.WIN_RANGE = 4
+        self.board = self.create_board()
+        self.turn = random.choice([self.USER, self.AI])
 
-    ##########################################################################
-    # CREATION OF BOARD AND GAME PLAY MECHANICS 
-    ##########################################################################
-    def print_board(self):
-        for row in self.board:
-            print(' '.join(row))
-        print(' '.join(map(str, range(self.cols))))
+    def create_board(self):
+        board = np.zeros((self.rows, self.columns))
+        return board
 
-    def drop_piece(self, col):
-        if col < 0 or col >= self.cols or self.board[0][col] != '-':
-            return False
-        for row in range(self.rows - 1, -1, -1):
-            if self.board[row][col] == '-':
-                self.board[row][col] = self.turn
-                break
-        return True
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
 
-    def check_winner(self):
-        # Horizontal, vertical and diagonal checks
-        for row in range(self.rows):
-            for col in range(self.cols - 3):
-                if self.board[row][col] == self.turn and all(self.board[row][col + i] == self.turn for i in range(4)):
+    def is_valid_location(self, board, col):
+        return board[self.rows - 1][col] == self.EMPTY
+
+    def get_next_open_row(self, board, col):
+        for r in range(self.rows):
+            if board[r][col] == self.EMPTY:
+                return r
+
+    def print_board(self, board):
+        print(np.flip(board, 0))
+
+    # check if any piece has won either player the game
+    def winning_move(self, board, piece):
+        for c in range(self.columns - self.WIN_RANGE + 1):
+            for r in range(self.rows):
+                if (board[r][c] == piece and board[r][c + 1] == piece and
+                    board[r][c + 2] == piece and board[r][c + 3] == piece):
                     return True
 
-        for col in range(self.cols):
-            for row in range(self.rows - 3):
-                if self.board[row][col] == self.turn and all(self.board[row + i][col] == self.turn for i in range(4)):
+        for c in range(self.columns):
+            for r in range(self.rows - self.WIN_RANGE + 1):
+                if (board[r][c] == piece and board[r + 1][c] == piece and
+                    board[r + 2][c] == piece and board[r + 3][c] == piece):
                     return True
 
-        for row in range(self.rows - 3):
-            for col in range(self.cols - 3):
-                if self.board[row][col] == self.turn and all(self.board[row + i][col + i] == self.turn for i in range(4)):
+        for c in range(self.columns - self.WIN_RANGE + 1):
+            for r in range(self.rows - self.WIN_RANGE + 1):
+                if (board[r][c] == piece and board[r + 1][c + 1] == piece and
+                    board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece):
                     return True
 
-                if self.board[row + 3][col] == self.turn and all(self.board[row + 3 - i][col + i] == self.turn for i in range(4)):
+                if (board[r + 3][c] == piece and board[r + 3 - 1][c + 1] == piece and
+                    board[r + 3 - 2][c + 2] == piece and board[r + 3 - 3][c + 3] == piece):
                     return True
+
         return False
     
-    def play_game(self, bot_play=True):
-        while True:
-            self.print_board()
-            if self.turn == 'X':
-                if bot_play:
-                    col, minimax_score = self.minimax(5, -math.inf, math.inf, True)
-                    if col is None:
-                        print("No valid moves left!")
-                        break
-                else:
-                    col = int(input(f"Player {self.turn}, choose column (0-{self.cols - 1}): "))
-            else:
-                col = int(input(f"Player {self.turn}, choose column (0-{self.cols - 1}): "))
+################################################################################    
+##################### MINIMAX AND ALPHA BETA PRUNING ALGORITHMS ################
+################################################################################
 
-            if self.drop_piece(col):
-                if self.check_winner():
-                    self.print_board()
-                    print(f"Player {self.turn} wins!")
-                    break
-                if self.is_full():
-                    self.print_board()
-                    print("It's a draw!")
-                    break
-                self.switch_turn()
-            else:
-                print("Invalid move, try again.")
-
-    ##########################################################################
-    # MINIMAX ALGO AND BOARD STATE EVAL
-    ##########################################################################
-    def is_full(self):
-        return all(self.board[0][col] != '-' for col in range(self.cols))
-
-    def switch_turn(self):
-        self.turn = 'O' if self.turn == 'X' else 'X'
-
-    def get_valid_locations(self):
-        return [col for col in range(self.cols) if self.board[0][col] == '-']
-
-    def is_terminal_node(self):
-        return self.check_winner() or self.is_full()
-    
     def evaluate_window(self, window, piece):
         score = 0
-        opp_piece = 'O' if piece == 'X' else 'X'
-        if window.count(piece) == 4:
+        opp_piece = self.USER_PIECE
+        if piece == self.USER_PIECE:
+            opp_piece = self.AI_PIECE
+
+        if window.count(piece) == self.WIN_RANGE:
             score += 100
-        elif window.count(piece) == 3 and window.count('-') == 1:
+        elif (window.count(piece) == self.WIN_RANGE - 1 and
+              window.count(self.EMPTY) == 1):
             score += 5
-        elif window.count(piece) == 2 and window.count('-') == 2:
+        elif (window.count(piece) == self.WIN_RANGE - 2 and
+              window.count(self.EMPTY) == 2):
             score += 2
 
-        if window.count(opp_piece) == 3 and window.count('-') == 1:
+        if (window.count(opp_piece) == self.WIN_RANGE - 1 and
+            window.count(self.EMPTY) == 1):
             score -= 4
 
         return score
 
-    def evaluate_board(self, piece):
+    def score_position(self, board, piece):
         score = 0
 
-        ## Score center column
-        center_array = [self.board[i][self.cols//2] for i in range(self.rows)]
+        center_array = [int(i) for i in list(board[:, self.columns // 2])]
         center_count = center_array.count(piece)
         score += center_count * 3
 
-        ## Score Horizontal
         for r in range(self.rows):
-            row_array = [self.board[r][c] for c in range(self.cols)]
-            for c in range(self.cols - 3):
-                window = row_array[c:c+4]
+            row_array = [int(i) for i in list(board[r, :])]
+            for c in range(self.columns - self.WIN_RANGE + 1):
+                window = row_array[c:c + self.WIN_RANGE]
                 score += self.evaluate_window(window, piece)
 
-        ## Score Vertical
-        for c in range(self.cols):
-            col_array = [self.board[r][c] for r in range(self.rows)]
-            for r in range(self.rows - 3):
-                window = col_array[r:r+4]
+        for c in range(self.columns):
+            col_array = [int(i) for i in list(board[:, c])]
+            for r in range(self.rows - self.WIN_RANGE + 1):
+                window = col_array[r:r + self.WIN_RANGE]
                 score += self.evaluate_window(window, piece)
 
-        ## Score positive sloped diagonal
-        for r in range(self.rows - 3):
-            for c in range(self.cols - 3):
-                window = [self.board[r+i][c+i] for i in range(4)]
+        for r in range(self.rows - self.WIN_RANGE + 1):
+            for c in range(self.columns - self.WIN_RANGE + 1):
+                window = [board[r + i][c + i] for i in range(self.WIN_RANGE)]
                 score += self.evaluate_window(window, piece)
 
-        ## Score negative sloped diagonal
-        for r in range(self.rows - 3):
-            for c in range(self.cols - 3):
-                window = [self.board[r+3-i][c+i] for i in range(4)]
+        for r in range(self.rows - self.WIN_RANGE + 1):
+            for c in range(self.columns - self.WIN_RANGE + 1):
+                window = [board[r + self.WIN_RANGE - 1 - i][c + i] for i in range(self.WIN_RANGE)]
                 score += self.evaluate_window(window, piece)
 
         return score
 
-    
-    def minimax(self, depth, alpha, beta, maximizingPlayer):
-        valid_locations = self.get_valid_locations()
-        is_terminal = self.is_terminal_node()
+    def is_terminal_node(self, board):
+        return (self.winning_move(board, self.USER_PIECE) or
+                self.winning_move(board, self.AI_PIECE) or
+                len(self.get_valid_locations(board)) == 0)
+
+    def minimax(self, board, depth, alpha, beta, maximizingPlayer):
+        valid_locations = self.get_valid_locations(board)
+        is_terminal = self.is_terminal_node(board)
         if depth == 0 or is_terminal:
             if is_terminal:
-                if self.check_winner():
-                    # If the current player is maximizing, then they have won
-                    return (None, float('inf') if maximizingPlayer else float('-inf'))
-                else: # Game is over, no more valid moves
+                if self.winning_move(board, self.AI_PIECE):
+                    return (None, 100000000000000)
+                elif self.winning_move(board, self.USER_PIECE):
+                    return (None, -10000000000000)
+                else:  # Game is over, no more valid moves
                     return (None, 0)
-            else: # Depth is zero
-                return (None, self.evaluate_board('X' if maximizingPlayer else 'O'))
+            else:  # Depth is zero
+                return (None, self.score_position(board, self.AI_PIECE))
         if maximizingPlayer:
-            value = float('-inf')
+            value = -math.inf
             column = random.choice(valid_locations)
             for col in valid_locations:
-                row = self.get_next_open_row(col)
-                b_copy = copy.deepcopy(self.board)
-                b_copy[row][col] = 'X'
-                new_score = self.minimax(depth-1, alpha, beta, False)[1]
+                row = self.get_next_open_row(board, col)
+                b_copy = np.copy(board)
+                self.drop_piece(b_copy, row, col, self.AI_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, False)[1]
                 if new_score > value:
                     value = new_score
                     column = col
@@ -168,14 +150,15 @@ class ConnectFour:
                 if alpha >= beta:
                     break
             return column, value
-        else: # Minimizing player
-            value = float('inf')
+
+        else:  # Minimizing player
+            value = math.inf
             column = random.choice(valid_locations)
             for col in valid_locations:
-                row = self.get_next_open_row(col)
-                b_copy = copy.deepcopy(self.board)
-                b_copy[row][col] = 'O'
-                new_score = self.minimax(depth-1, alpha, beta, True)[1]
+                row = self.get_next_open_row(board, col)
+                b_copy = np.copy(board)
+                self.drop_piece(b_copy, row, col, self.USER_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, True)[1]
                 if new_score < value:
                     value = new_score
                     column = col
@@ -184,14 +167,75 @@ class ConnectFour:
                     break
             return column, value
 
-    def get_next_open_row(self, col):
-        for r in range(self.rows-1, -1, -1):
-            if self.board[r][col] == '-':
-                return r
-        raise Exception("Column is full")
-            
+    def get_valid_locations(self, board):
+        valid_locations = []
+        for col in range(self.columns):
+            if self.is_valid_location(board, col):
+                valid_locations.append(col)
+        return valid_locations
 
+    def pick_best_move(self, board, piece):
+        valid_locations = self.get_valid_locations(board)
+        best_score = -10000
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = self.get_next_open_row(board, col)
+            temp_board = np.copy(board)
+            self.drop_piece(temp_board, row, col, piece)
+            score = self.score_position(temp_board, piece)
+            if score > best_score:
+                best_score = score
+                best_col = col
+
+        return best_col
+    
+################################################################################    
+########################## PLAYING THE ACTUAL GAME #############################
+################################################################################
+
+    def draw_board(self, board):
+        for r in range(self.rows):
+            for c in range(self.columns):
+                if board[r][c] == self.USER_PIECE:
+                    print("P ", end="")
+                elif board[r][c] == self.AI_PIECE:
+                    print("AI ", end="")
+                else:
+                    print("- ", end="")
+            print()
+        print()
+
+    def play_game(self):
+        game_over = False
+
+        while not game_over:
+            self.print_board(self.board)
+
+            if self.turn == self.USER:
+                try:
+                    col = int(input(f"Player 1, enter column (0-{self.columns - 1}): "))
+                    if not self.is_valid_location(self.board, col):
+                        print("Invalid move. Try again.")
+                        continue
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+                    continue
+            else:
+                col, _ = self.minimax(self.board, 5, -sys.maxsize, sys.maxsize, True)
+
+            row = self.get_next_open_row(self.board, col)
+            self.drop_piece(self.board, row, col, self.turn + 1)
+
+            if self.winning_move(self.board, self.turn + 1):
+                self.print_board(self.board)
+                print(f"Player {self.turn + 1} wins!!")
+                game_over = True
+            elif len(self.get_valid_locations(self.board)) == 0:
+                print("It's a draw!")
+                game_over = True
+            print("-" * (self.columns * 4))  
+            self.turn = 1 - self.turn
 
 if __name__ == '__main__':
-    game = ConnectFour()
-    game.play_game()
+    connect = ConnectFour(rows=6, columns=7)
+    connect.play_game()
